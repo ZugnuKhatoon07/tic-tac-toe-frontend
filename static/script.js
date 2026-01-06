e092f654d6ab7ce3480d37a307bca0cd661f7a0e
Zugnukhatoon07 <02zugnu2005@gmail.com>
2026-01-06 12:17:28 +0530

    added room system and multiplayer fix

--- static/script.js
+++ static/script.js
@@ -1,46 +1,68 @@
 const socket = io("https://tic-tac-toe-backend-69kg.onrender.com");
+
 const boardDiv = document.getElementById("board");
 const turnText = document.getElementById("turn");
 
 let cells = [];
 let soundOn = true;
 
-const clickSound = new Audio("https://www.soundjay.com/buttons/sounds/button-16.mp3");
+const clickSound = new Audio(
+  "https://www.soundjay.com/buttons/sounds/button-16.mp3"
+);
 
+// 🔑 ROOM & PLAYER
+const room = prompt("Room code daalo (same dono log use kare):");
+const player = prompt("X ya O likho:");
+
+socket.emit("join", { room, player });
+
+// 🧩 BOARD CREATE
 for (let i = 0; i < 9; i++) {
-    let div = document.createElement("div");
-    div.classList.add("cell");
-    div.onclick = () => {
-        if (soundOn) clickSound.play();
-        socket.emit("move", { index: i });
-    };
-    boardDiv.appendChild(div);
-    cells.push(div);
+  let div = document.createElement("div");
+  div.classList.add("cell");
+  div.onclick = () => {
+    if (soundOn) clickSound.play();
+    socket.emit("move", {
+      room: room,
+      index: i,
+      player: player
+    });
+  };
+  boardDiv.appendChild(div);
+  cells.push(div);
 }
 
-socket.on("update", data => {
-    data.board.forEach((val, i) => {
-        cells[i].innerText = val;
-        cells[i].className = "cell " + (val === "X" ? "x" : "o");
-    });
-    turnText.innerText = "Player " + data.player + " Turn";
+// 🔁 UPDATE BOARD
+socket.on("move", data => {
+  data.board.forEach((val, i) => {
+    cells[i].innerText = val;
+    cells[i].className = "cell " + (val === "X" ? "x" : "o");
+  });
+  turnText.innerText = "Player " + data.turn + " Turn";
 });
 
-// 🔄 Restart Game
+// 🔄 Restart
 function restartGame() {
-    location.reload();
+  socket.emit("restart", { room });
 }
 
-// 🎨 Theme Change
+// 🔄 Restart response
+socket.on("restart", () => {
+  cells.forEach(c => (c.innerText = ""));
+  turnText.innerText = "Game Restarted";
+});
+
+// 🎨 Theme
 function changeTheme() {
-    document.body.classList.toggle("dark");
+  document.body.classList.toggle("dark");
 }
 
-// 🔊 Sound ON/OFF
+// 🔊 Sound
 function toggleSound() {
-    soundOn = !soundOn;
-    alert(soundOn ? "Sound ON 🔊" : "Sound OFF 🔕");
+  soundOn = !soundOn;
+  alert(soundOn ? "Sound ON 🔊" : "Sound OFF 🔕");
 }
+
 
 
 //const socket = io("https://tic-tac-toe-backend-69kg.onrender.com"); 
